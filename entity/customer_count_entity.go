d97b7b4af87bd874510bcd85c46f265b484f4e29
Daniel Adhitthana <dadhitthana@gmail.com>
2025-08-25 14:59:55 +0700

    fix: customer count response

--- entity/customer_count_entity.go
+++ entity/customer_count_entity.go
@@ -14,9 +14,9 @@ const (
 )
 
 type CustomerCount struct {
-	Timestamp  time.Time `gorm:"primaryKey;not null" validate:"required"`
+	Timestamp  time.Time `gorm:"primaryKey;not null" json:"timestamp" validate:"required"`
 	LocationID uuid.UUID `gorm:"type:uuid;primaryKey;not null" json:"location_id" validate:"required,uuid"`
-	Location   Location  `gorm:"foreignKey:LocationID;references:ID"`
+	Location   Location  `gorm:"foreignKey:LocationID;references:ID" json:"-"`
 	Gender     Gender    `gorm:"type:varchar(1);primaryKey;not null" json:"gender" validate:"required"`
 	Count      uint64    `gorm:"not null" json:"count" validate:"required"`
 }
